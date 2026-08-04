5227309c41abde85a8720a5b0a23a526e86336aa
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 03:43:23 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,8 +26,6 @@ require "kettle/test/rspec"
 # This library
 require "ruby-openid2"
 
-require "openid"
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
